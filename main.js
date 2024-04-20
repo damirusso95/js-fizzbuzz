e0f5d83cd55a1b0212e5ab2fda791cabe887679f
damirusso95 <damirusso95@gmail.com>
2024-04-20 11:56:20 +0200

    fizzbuzz multipli di 3 e 5

--- main.js
+++ main.js
@@ -10,24 +10,32 @@
 // 1) Scrivi un programma che stampi in console i numeri da 1 a 100                 ----> ok
 // 2) per i multipli di 3 stampi “Fizz” al posto del numero                         ----> ok
 // 3) per i multipli di 5 stampi “Buzz”                                             ----> ok
-// 4) Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+// 4) Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.           ----> ok   
 
 let i = 1
 // stampo in console numeri da 1 a 100 utilizzando il ciclo for
 for(let i = 1; i <= 100; i++){
     console.log(i)
-// stampo "fizz" al posto dei numeri multipli di 3
-    if (i % 3 ==0) {
-        console.log(i,"Fizz")
-    }
 
-    else if(i % 5 ==0){
-        console.log(i,"Buzz")
-    }
+// stampo Bazz al posto dei numeri multipli di 5 e 3
+if(i % 5 ==0 && i % 3 ==0){
+    console.log(i,"FizzBuzz")
+}
+
+// stampo "fizzBuzz" al posto dei numeri multipli di 3
+else if(i % 3 ==0){
+    console.log(i,"Fizz")
+    
+}
+
+// stampo Fizz al posto dei numeri multipli di 5
+ else if(i % 5 ==0){
+    console.log(i,"Buzz")
+}
 
 
 
-
+    
 
 
 
