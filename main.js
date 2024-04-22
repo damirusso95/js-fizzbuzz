2a2b98f6b588f263ddf475883c17de5bc0afd37e
damirusso95 <damirusso95@gmail.com>
2024-04-22 10:53:32 +0200

    stampo su schermo

--- main.js
+++ main.js
@@ -11,36 +11,37 @@
 // 2) per i multipli di 3 stampi “Fizz” al posto del numero                         ----> ok
 // 3) per i multipli di 5 stampi “Buzz”                                             ----> ok
 // 4) Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.           ----> ok   
-
+const item = document.querySelector('ul');
 let i = 1
 // stampo in console numeri da 1 a 100 utilizzando il ciclo for
 for( i ; i <= 100; i++){
+    let valore = i;
     
 // stampo Bazz al posto dei numeri multipli di 5 e 3
 if(i % 5 ==0 && i % 3 ==0){
     console.log(i,"FizzBuzz")
+    valore = `fizzBuzz`
+    
+   
 }
-
 // stampo "fizzBuzz" al posto dei numeri multipli di 3
 else if(i % 3 ==0){
     console.log(i,"Fizz")
+    valore = `fizz`
     
 }
 
 // stampo Fizz al posto dei numeri multipli di 5
  else if(i % 5 ==0){
     console.log(i,"Buzz")
+    valore = `buzz`
 }
 
 else{
     console.log(i);
 }
 
-
-
-    
-
-
+item.innerHTML = item.innerHTML + `<li>${valore}<li/>`;
 
 }
 
